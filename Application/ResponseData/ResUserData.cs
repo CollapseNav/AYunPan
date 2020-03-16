f43eca41dc64d1d690001364086f4d039e76c178
CollapseNav <jwpwdwd@163.com>
2020-03-16 17:17:06 +0800

    暂定返回的数据结构

---
+++ Application/ResponseData/ResUserData.cs
@@ -0,0 +1,33 @@
+using Application.Core.BaseRequestData;
+using Application.Core.BaseResponseData;
+using Repository.Domain;
+
+namespace Application.ResponseData {
+    public class ResUserData : IResponseData<UserDataInfo> {
+        public string Id { get; set; }
+        public string UserAccount { get; set; }
+        public string UserName { get; set; }
+        public string Age { get; set; }
+        public string Gender { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string CreateDate { get; set; }
+        public string Remark { get; set; }
+        public string Cap { get; set; }
+        public string Stored { get; set; }
+
+        public ResUserData (UserDataInfo user) {
+            Id = user.Id;
+            UserAccount = user.UserAccount;
+            UserName = user.UserName;
+            Age = user.Age.ToString ();
+            Gender = user.Gender;
+            Email = user.EmailAddress;
+            Phone = user.Phone;
+            CreateDate = user.CreateDate.ToString ();
+            Remark = user.Remark;
+            Cap = user.Cap;
+            Stored = user.Stored;
+        }
+    }
+}
