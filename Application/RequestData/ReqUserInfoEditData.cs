183611368d797140161c0ff2cf4d5d89ef422817
CollapseNav <jwpwdwd@163.com>
2020-03-16 17:16:37 +0800

    请求的数据结构

--- Application/RequestData/ReqUserInfoEditData.cs
+++ Application/RequestData/ReqUserInfoEditData.cs
@@ -2,7 +2,7 @@
  * @Author: CollapseNav
  * @Date: 2019-12-30 22:15:28
  * @LastEditors: CollapseNav
- * @LastEditTime: 2020-03-04 18:49:10
+ * @LastEditTime: 2020-03-12 22:06:24
  * @Description: 
  */
 using System;
@@ -12,10 +12,9 @@ using Repository.Domain;
 
 namespace Application.RequestData {
     public class ReqUserInfoEditData : IRequestEditData<UserDataInfo> {
-        public string UserAccount { get; set; }
         public string UserName { get; set; }
         public string UserId { get; set; }
-        public string Gender { get; set; }
+        public int Gender { get; set; }
         public string Age { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
@@ -23,10 +22,9 @@ namespace Application.RequestData {
 
         public Expression<Func<UserDataInfo, UserDataInfo>> GetConvertExpressions () {
             return u => new UserDataInfo {
-                UserAccount = UserAccount,
                 UserName = UserName,
                 ChangedBy = UserId,
-                Gender = Gender,
+                Gender = Gender.ToString (),
                 Age = int.Parse (Age),
                 EmailAddress = Email,
                 Phone = Phone,
