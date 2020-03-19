7de3f140857662a4ffb2ad46b6cca5d0880ad183
CollapseNav <jwpwdwd@163.com>
2020-03-20 00:18:03 +0800

    主要是添加了一个下载的功能，然后搞了个让我很纠结的目录配置

--- Api/Controllers/UserFileController.cs
+++ Api/Controllers/UserFileController.cs
@@ -14,19 +14,30 @@ using Application.ResponseData;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
 using Repository.Domain;
 
 namespace Api.Controllers {
+    public class test {
+        public string id { get; set; }
+    }
+
     [ApiController]
     [Route ("api/[controller]")]
     public class UserFileController : ControllerBase {
         private readonly UserFileApplication app;
         private readonly UserDataApplication user;
+        private readonly IConfiguration config;
+        private readonly string DirectoryPath;
         public UserFileController (UserFileApplication _app, UserDataApplication _user) {
             user = _user;
             app = _app;
+            var builder = new ConfigurationBuilder ().SetBasePath (Directory.GetCurrentDirectory ()).AddJsonFile ("appsettings.json");
+            config = builder.Build ();
+            DirectoryPath = Directory.GetCurrentDirectory () + config["FileStore"];
         }
 
         [HttpPost, Route ("[action]")]
@@ -37,7 +48,7 @@ namespace Api.Controllers {
             var udata = user.GetFullUserDataById (id);
             var rootFolder = app.GetFileByFileId (rootId);
             // 若用户目录不存在，则新建一个
-            if (!Directory.Exists (Directory.GetCurrentDirectory () + udata.FolderPath)) {
+            if (!Directory.Exists (DirectoryPath + udata.FolderPath)) {
                 rootFolder = new Repository.Domain.FileInfo {
                     FileName = udata.UserAccount,
                     FileType = EFileType.folder.ToString (),
@@ -49,7 +60,7 @@ namespace Api.Controllers {
                     Shared = 0
                 };
                 app.AddFile (rootFolder);
-                Directory.CreateDirectory (Directory.GetCurrentDirectory () + udata.FolderPath);
+                Directory.CreateDirectory (DirectoryPath + udata.FolderPath);
             }
             try {
                 var data = new Repository.Domain.FileInfo {
@@ -62,10 +73,15 @@ namespace Api.Controllers {
                     MapPath = rootFolder.FilePath,
                     Shared = 0
                 };
-                using FileStream fs = new FileStream (Directory.GetCurrentDirectory () + "/" + data.FilePath, FileMode.CreateNew);
-                await file.CopyToAsync (fs);
-                app.AddFile (data);
-                return Ok (new ResUserFiles (data));
+                string truepath = DirectoryPath + "/" + data.FilePath;
+                if (!Directory.Exists (truepath)) {
+                    using FileStream fs = new FileStream (DirectoryPath + "/" + data.FilePath, FileMode.CreateNew);
+                    await file.CopyToAsync (fs);
+                    app.AddFile (data);
+                    return Ok (new ResUserFiles (data));
+                } else {
+                    return BadRequest ("当前目录已存在同名文件");
+                }
             } catch (Exception ex) {
                 Console.WriteLine (ex.Message);
                 return BadRequest ();
@@ -78,23 +94,48 @@ namespace Api.Controllers {
             return Ok (item);
         }
 
+        [HttpPost, Route ("[action]"), EnableCors ("File")]
+        public IActionResult DownloadFile (ReqDownload data) {
+            var file = app.GetFileByFileId (data.Id);
+            var ext = file.FileName.Split ('.') [ ^ 1];
+            Response.Headers.Add ("FileName",
+                System.Web.HttpUtility.UrlEncode (file.FileName, Encoding.UTF8));
+            return File (new FileStream (DirectoryPath + file.FilePath, FileMode.Open), new FileExtensionContentTypeProvider ().Mappings['.' + ext], file.FileName);
+        }
+
+        [HttpPost, Route ("[action]")]
+        public IActionResult DownloadFile2 (ReqDownload data) {
+            var file = app.GetFileByFileId (data.Id);
+            var ext = file.FileName.Split ('.') [ ^ 1];
+            return Ok (new {
+                file = new FileStream (DirectoryPath + file.FilePath, FileMode.Open),
+                    ext
+            });
+        }
+
         [HttpPost, Route ("[action]")]
         public IActionResult CreateNewFolder (ReqNewFolder folder) {
             var root = app.GetFileByFileId (folder.RootId);
             try {
-                var data = new Repository.Domain.FileInfo {
-                    FileName = folder.FolderName,
-                    FilePath = root.FilePath + '/' + folder.FolderName,
-                    MapPath = root.FilePath,
-                    FileType = EFileType.folder.ToString (),
-                    FileSize = "0",
-                    OwnerId = root.OwnerId,
-                    OwnerName = root.OwnerName,
-                    Shared = 0
-                };
-                app.AddFile (data);
-                Directory.CreateDirectory (Directory.GetCurrentDirectory () + root.FilePath + '/' + folder.FolderName);
-                return Ok (new ResUserFiles (data) { FileContains = new List<ResUserFiles> () });
+                string folderpath = root.FilePath + '/' + folder.FolderName;
+                string truepath = DirectoryPath + folderpath;
+                if (!Directory.Exists (truepath)) {
+                    var data = new Repository.Domain.FileInfo {
+                        FileName = folder.FolderName,
+                        FilePath = folderpath,
+                        MapPath = root.FilePath,
+                        FileType = EFileType.folder.ToString (),
+                        FileSize = "0",
+                        OwnerId = root.OwnerId,
+                        OwnerName = root.OwnerName,
+                        Shared = 0
+                    };
+                    app.AddFile (data);
+                    Directory.CreateDirectory (truepath);
+                    return Ok (new ResUserFiles (data) { FileContains = new List<ResUserFiles> () });
+                } else {
+                    return BadRequest ("当前目录已存在同名文件夹");
+                }
             } catch (Exception ex) {
                 return BadRequest (ex.Message);
             }
